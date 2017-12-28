b20db6876d3fd0e80b9ca8c2daaa60f13cc4ee41
aiodsunil <aiodsunil@gmail.com>
2017-12-28 00:36:10 +0000

    update code with junit5

--- src/test/java/com/event/KafkaTests.java
+++ src/test/java/com/event/KafkaTests.java
@@ -1,18 +1,11 @@
 package com.event;
 
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-public class ApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class KafkaTests {
 }
